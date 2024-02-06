b3635822f7d46c51157c8ec0719a7f3e8b26950e
Darielvjo <20230186@itla.edu.do>
2024-02-05 23:11:45 -0400

    Actualizacion Entidad Libro

---
+++ Library/Library.Domain/Entities/Libro.cs
@@ -0,0 +1,16 @@
+﻿using Library.Domain.Core;
+
+namespace Library.Domain.Entities
+{
+    internal class Libro : BaseEntity
+    {
+        public int IdLibro { get; set; }
+        public int? IdCategoria { get; set; }
+        public int? Ejemplares { get; set; }
+        public string? Titulo { get; set; }
+        public string? Autor { get; set; }
+        public string? Editorial { get; set; }
+        public string? Ubicacion { get; set; }
+        public string? Portada { get; set; }
+    }
+}
