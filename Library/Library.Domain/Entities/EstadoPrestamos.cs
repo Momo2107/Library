7de943887f7ea4a410d4de8e97c1c1ebe38e2f26
wabish0 <francismateo444@gmail.com>
2024-02-10 01:22:41 -0400

    actualizacion
    Actualizacion
    Creacion de calse EstadoPrestamo

---
+++ Library/Library.Domain/Entities/EstadoPrestamos.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace Library.Domain.Entities
+{
+    public class EstadoPrestamos
+    {
+        public int? IdEstadoPrestamo { get; set; }
+        public string? Descripcion { get; set; }
+
+    }
+}
