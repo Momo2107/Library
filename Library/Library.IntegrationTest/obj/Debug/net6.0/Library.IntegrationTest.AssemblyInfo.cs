9946f4592c99f1c621b7652283374ec752256956
wabish0 <francismateo444@gmail.com>
2024-02-24 09:55:13 -0400

    Arreglo
    actualización

--- Library/Library.IntegrationTest/obj/Debug/net6.0/Library.IntegrationTest.AssemblyInfo.cs
+++ Library/Library.IntegrationTest/obj/Debug/net6.0/Library.IntegrationTest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Library.IntegrationTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87dab227fc2205bfb137e7976ade8767837fced7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+353d07d7dc3213fb861c55e9712fe4a51d1bbf71")]
 [assembly: System.Reflection.AssemblyProductAttribute("Library.IntegrationTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Library.IntegrationTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
